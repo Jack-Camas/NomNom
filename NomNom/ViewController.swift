1be7a012688f5c3b23f01a7c524953f9a48e5d49
Jack-Camas <jackcamasdev@gmail.com>
2022-12-08 13:25:56 -0500

    Initial Commit

---
+++ NomNom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NomNom
+//
+//  Created by Jack Camas on 12/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
